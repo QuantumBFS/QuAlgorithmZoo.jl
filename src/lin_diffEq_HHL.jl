bd40f0d03d33df65498a3789cde7b72d9a7e3255
Divyanshu Gupta <guptadv17@gmail.com>
2019-03-17 09:31:25 +0530

    Added Higher-order linear multistep

--- src/lin_diffEq_HHL.jl
+++ src/lin_diffEq_HHL.jl
@@ -1,10 +1,12 @@
-export Array_QuEuler, prepare_init_state, solve_QuEuler
+export Array_QuDiff, prepare_init_state, solve_QuDiff, alg_lin_diffeq, bval, Aval
+export QuEuler, QuLeapfrog, QuAB2, QuAB3, QuAB4
 
 """
     Based on : arxiv.org/abs/1010.2745v2
 
-    QuArray_Euler(N_t,N,h,A)
-    prepare_init_state(b,x,h,N_t)
+    * Array_QuDiff(N_t,N,h,A) - generates matrix for k-step solver
+    * prepare_init_state(b,x,h,N_t) - generates inital states
+    * solve_QuDiff - solver
 
     x' = Ax + b
 
@@ -15,13 +17,53 @@ export Array_QuEuler, prepare_init_state, solve_QuEuler
     * h - step size.
     * tspan - time span.
 """
-function prepare_init_state(tspan::Tuple,x::Vector,h::Float64,g::Function)
+
+"""
+    alg_lin_diffeq
+    * step - step for multistep method
+    * α - coefficients for xₙ
+    * β - coefficent for xₙ'
+"""
+struct alg_lin_diffeq
+  step::Int
+  α::Array
+  β::Array
+  alg_lin_diffeq(step,α,β) = new(step,α,β)
+end
+
+function bval(alg::alg_lin_diffeq,t,h,g::Function)
+  b = zero(g(1))
+  for i = 1:(alg.step)
+    b += alg.β[i]*g(t-(i-1)*h)
+  end
+  b
+end
+
+function Aval(alg::alg_lin_diffeq,t,h,g::Function)
+  I_mat = Matrix{Float64}(I, size(g(1)))
+  A = Matrix{Float64}(I, size(g(1)))
+  for i = 1:(alg.step)
+    A = [-1*(alg.α[i]*I_mat + h*alg.β[i]*g(t-(i-1)*h)) A]
+  end
+  A
+end
+"""
+    Explicit Linear Multistep Methods
+"""
+QuEuler = alg_lin_diffeq(1,[1],[1]);
+QuLeapfrog = alg_lin_diffeq(2,[0 1],[2 0])
+QuAB2 = alg_lin_diffeq(2,[1 0], [1.5 -0.5])
+QuAB3 = alg_lin_diffeq(3,[1 0 0], [23/12 -16/12 5/12])
+QuAB4 = alg_lin_diffeq(4,[1 0 0 0], [55/24 -59/24 37/24 -9/24])
+
+
+function prepare_init_state(tspan::Tuple,x::Vector,h::Float64,g::Function,alg::alg_lin_diffeq)
   init_state = x;
-  N_t = round(2*(tspan[2] - tspan[1])/h + 3)
+  N_t = Int(round(2*(tspan[2] - tspan[1])/h + 3))
   b = similar(g(1))
   for i = 1:N_t
-   if i < (N_t+1)/2
-      b = g(h*i + tspan[1])
+   if i < (N_t+1)/2 -1
+      b = bval(alg,h*(i-1) + tspan[1],h,g)
       init_state = [init_state;h*b]
     else
       init_state = [init_state;zero(b)]
@@ -32,51 +74,48 @@ function prepare_init_state(tspan::Tuple,x::Vector,h::Float64,g::Function)
   init_state
 end
 
-function Array_QuEuler(tspan::Tuple,h::Float64,g::Function)
-  N_t = round(2*(tspan[2] - tspan[1])/h + 3)
+function Array_QuDiff(tspan::Tuple,h::Float64,g::Function,alg::alg_lin_diffeq)
+  N_t = Int(round(2*(tspan[2] - tspan[1])/h + 3))
+  sz = size(g(1))
   I_mat = Matrix{Float64}(I, size(g(1)));
   A_ = I_mat;
-  zero_mat = zero(I_mat);
-  tmp_A = Array{Float64,2}(undef,size(g(1)))
-
-  for j = 2: N_t +1
-    A_ = [A_ zero_mat]
+  # Generates First two rows
+  A_ = [A_ zeros(sz[1],sz[1]*(N_t));-1*(I_mat + h*g(tspan[1])) I_mat zeros(sz[1],sz[1]*(N_t-1))]
+  #Generates additional rows based on k - step
+  if alg.step > 1
+    A_ = [A_; Aval(QuAB2,2*h + tspan[1],h,g) zeros(sz[1],sz[1]*(N_t-2))]
   end
 
-  for i = 2 : N_t+1
-    tmp_A =  g(i*h + tspan[1])
-    tmp_A = -1*(I_mat + h*tmp_A)
-    tA_ = copy(zero_mat)
-    if i<3
-      tA_ = [tmp_A I_mat]
-    else
-      for j = 1:i-3
-        tA_ = [tA_ zero_mat]
-      end
-      if i < (N_t + 1)/2 + 1
-        tA_ = [tA_ tmp_A I_mat]
-      else
-        tA_ = [tA_ -1*I_mat I_mat]
-      end
-    end
-    if i< N_t+1
-      for j = i+1: N_t+1
-        tA_ = [tA_ zero_mat]
-      end
-    end
-    A_ = [A_;tA_]
-
+  for i = 3:alg.step-1
+    tA_ = Aval(QuAB2,(i-1)*h + tspan[1],h,g)
+    tA_ = [zeros(sz[1],sz[1]*(i-2)) tA_]
+    A_ = [A_; tA_ zeros(sz[1],sz[1]*(N_t-i))]
   end
-    A_ = [zero(A_) A_;A_' zero(A_)]
-    A_
+
+  if alg.step > 2
+    A_ = [A_;Aval(alg,(alg.step)*h + tspan[1],h,g) zeros(sz[1],sz[1]*(N_t-alg.step)) ]
+  end
+
+  for i = alg.step+1: (N_t + 1)/2 -2
+    tA_ = Aval(alg,(i-1)*h + tspan[1],h,g)
+    A_= [A_;zeros(sz[1],Int(sz[1]*(i-alg.step))) tA_ zeros(sz[1],Int(sz[1]*(N_t-i)))]
+   end
+  #Generates half mirroring matrix
+  for i = (N_t + 1)/2  : N_t
+    A_ = [A_; zeros(sz[1],Int(sz[1]*(i-2))) -1*I_mat I_mat zeros(sz[1],Int(sz[1]*(N_t + 1 -i)))]
+  end
+  A_ = [A_; zeros(sz[1],sz[1]*(N_t-1)) -1*I_mat I_mat];
+
+  A_ = [zero(A_) A_;A_' zero(A_)]
+  A_
 end
 
-function solve_QuEuler(A::Function, b::Function, x::Vector,tspan::Tuple, h::Float64)
+function solve_QuDiff(alg::alg_lin_diffeq ,A::Function, b::Function, x::Vector,tspan::Tuple, h::Float64)
 
-  mat = Array_QuEuler(tspan,h,A)
-  state = prepare_init_state(tspan,x,h,b)
+  mat = Array_QuDiff(tspan,h,A,alg)
+  state = prepare_init_state(tspan,x,h,b,alg)
   λ = maximum(eigvals(mat))
-  C_value = minimum(eigvals(mat) .|> abs)*0.1;
+  C_value = minimum(eigvals(mat) .|> abs)*0.01;
   n_reg = 12;
   mat = 1/(λ*2)*mat
   state = state*1/(2*λ) |> normalize!
--- test/lin_diffEq_test.jl
+++ test/lin_diffEq_test.jl
@@ -13,20 +13,42 @@ function diffEq_problem(nbit::Int)
     A, b, x
 end
 
-@testset "Linear_differential_equation_Euler_HHL" begin
+@testset "Linear_differential_equation_HHL" begin
 N = 1
-h = 0.02
+h = 0.1
 tspan = (0.0,0.6)
+N_t = round(2*(tspan[2] - tspan[1])/h + 3)
 M, v, x = diffEq_problem(N)
 A(t) = M
 b(t) = v
-res = solve_QuEuler(A, b, x, tspan, h)
 
 f(u,p,t) = M*u + v;
 prob = ODEProblem(f,x,tspan)
-sol = solve(prob,Euler(),dt = 0.02,adaptive = :false)
+
+sol = solve(prob,Tsit5(),dt = h,adaptive = :false)
 s = vcat(sol.u...)
-N_t = round(2*(tspan[2] - tspan[1])/h + 3);
-r = res[Int((N_t+1)*2+2^N+1): Int((N_t+1)*2+2^N + N_t - 1)] # range of relevant values in the obtained state.
-@test isapprox.(s,r,atol = 0.05) |> all
+
+res = solve_QuDiff(QuEuler,A, b, x, tspan, h)
+r = res[Int((N_t+1)*2+2^N -1): Int((N_t+1)*2+2^N + N_t - 3)] # range of relevant values in the obtained state.
+@test isapprox.(s,r,atol = 0.5) |> all
+
+res = solve_QuDiff(QuLeapfrog,A, b, x, tspan, h)
+r = res[Int((N_t+1)*2+2^N -1): Int((N_t+1)*2+2^N + N_t - 3)] # range of relevant values in the obtained state.
+@test isapprox.(s,r,atol = 0.3) |> all
+
+res = solve_QuDiff(QuAB2,A, b, x, tspan, h)
+r = res[Int((N_t+1)*2+2^N -1): Int((N_t+1)*2+2^N + N_t - 3)] # range of relevant values in the obtained state.
+@test isapprox.(s,r,atol = 0.3) |> all
+
+sol = solve(prob,Tsit5(),dt = h,adaptive = :false)
+s = vcat(sol.u...)
+
+res = solve_QuDiff(QuAB3,A, b, x, tspan, h)
+r = res[Int((N_t+1)*2+2^N -1): Int((N_t+1)*2+2^N + N_t - 3)] # range of relevant values in the obtained state.
+@test isapprox.(s,r,atol = 0.3) |> all
+
+res = solve_QuDiff(QuAB4,A, b, x, tspan, h)
+r = res[Int((N_t+1)*2+2^N -1): Int((N_t+1)*2+2^N + N_t - 3)] # range of relevant values in the obtained state.
+@test isapprox.(s,r,atol = 0.3) |> all
+
 end
