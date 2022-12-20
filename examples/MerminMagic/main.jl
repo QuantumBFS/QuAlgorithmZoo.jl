6487b2f2aff9abc00e3280154fa5ccdd0a5df512
Yusheng Zhao <yushengzhao2020@outlook.com>
2022-12-20 10:36:03 +0800

    clean up code

--- examples/MerminMagic/main.jl
+++ examples/MerminMagic/main.jl
@@ -4,19 +4,24 @@ using Yao
 
 # measurement operator table
 # see Table S1 in https://arxiv.org/abs/2206.12042
-op_mtx = [kron(I2, Z) kron(Z, I2) kron(Z, Z); kron(X, I2) kron(I2, X) kron(X, X); kron(-X, Z) kron(-Z, X) kron(Y, Y)]
+const op_mtx::Matrix{KronBlock} = [kron(I2, Z) kron(Z, I2) kron(Z, Z)
+    kron(X, I2) kron(I2, X) kron(X, X)
+    kron(-X, Z) kron(-Z, X) kron(Y, Y)]
 
 """
-    bell_state(which::Int)::AbstractRegister
+    bell_state(which::Int) -> AbstractRegister
 
 Initialize bell state on a quantum register.
 
 # Arguments
-- 'which::Int': which of the four bell states to initialize
+- `which::Int`: which of the four bell states to initialize
   1 corresponds to ``(|00> + |11>)/√2``
   2 corresponds to ``(|00> - |11>)/√2``
   3 corresponds to ``(|01> + |10>)/√2``
   4 corresponds to ``(|01> - |10>)/√2``
+
+# Returns
+- `AbstractRegister`: the register on which we have initialized the bell state
 """
 function bell_state(which::Int)
 
@@ -28,42 +33,19 @@ function bell_state(which::Int)
     return reg
 end
 
-"""
-    init_double_bell_state()::AbstractRegister
-
-Initialize two pairs of bell state ``(|00> + |11>) ⊗ (|00> + |11>)/2``.
-
-First qubit of each bell state pair belongs to Alice and
-the others belong to Bob.
-"""
-function init_double_bell_state()
-    return join(bell_state(1), bell_state(1))
-end
-
+"Alice perform Measurement using Operator specified by row idx"
+alice_measure!(reg::AbstractRegister, row::Int) = [real(measure!(op, reg, (1, 3))) for op in op_mtx[row, :]]
+"Bob perform Measurement using Operator specified by row idx"
+bob_measure!(reg::AbstractRegister, col::Int) = [real(measure!(op, reg, (2, 4))) for op in op_mtx[:, col]]
 
 """
-    meas_by_idx(reg::AbstractRegister, i::Int, j::Int)::Vector{Float64}
-
-Perform Measurement based on index assigned by dealer.
+    input_idx(which::String) -> Int
 
 # Arguments
-- `reg`: is the quantum register to measure
-- 'i::Int': the row idx assigned
-- 'j::Int': the column idx assigned
+- `which::String`: whether user is asked to provide the index for the row or column
 
-"""
-function meas_by_idx(reg::AbstractRegister, i::Int, j::Int)
-
-    alice_measure!(reg::AbstractRegister, row::Int) = [real(measure!(op, reg, (1, 3))) for op in op_mtx[row, :]]
-    bob_measure!(reg::AbstractRegister, col::Int) = [real(measure!(op, reg, (2, 4))) for op in op_mtx[:, col]]
-    return [alice_measure!(reg, i)'..., bob_measure!(reg, j)'...]
-end
-
-"""
-    input_idx(which::String)::Int
-
-# Arguments
-- 'which::String': whether user is providing index of the row or column
+# Returns
+- `Int`: index determined by the user
 """
 function input_idx(which::String)
     idx = 0
@@ -79,30 +61,36 @@ function input_idx(which::String)
 end
 
 function main()
-    # Dealer randomly generate a pair of numbers (i,j)
-    # i is the row number given to Alice
-    # j is the column number given to Bob
     println("Dealer: Let's Play Mermin's Magic Square Game!")
 
+    # ask the user to input row and column index
     i = input_idx("row")
     j = input_idx("column")
 
     println("Alice will provide three numbers to be written on row: $(i)")
     println("Bob will provide three numbers to be written on column: $(j)")
 
-    # initialize register to two pairs of bell state
-    reg1 = init_double_bell_state()
+    # Initialize two pairs of bell state ``(|00> + |11>) ⊗ (|00> + |11>)/2``.
+    # First qubit of each bell state pair belongs to Alice and the others belong to Bob.
+    reg1 = join(bell_state(1), bell_state(1))
 
     # do measurement and get answer string
-    a1, a2, a3, b1, b2, b3 = meas_by_idx(reg1, i, j)
+    a1, a2, a3 = alice_measure!(reg1, i)
+    b1, b2, b3 = bob_measure!(reg1, j)
 
     println("Alice should give answer: $a1 , $a2 , $a3")
     println("Bob should give answer: $b1 , $b2 , $b3")
-    println("We should see $([a1,a2,a3][j]) is equal to $([b1,b2,b3][i])")
-    @assert [a1, a2, 1 * a1 * a2][j] == [b1, b2, -1 * b1 * b2][i]
+    #We should see j_th element  in [a1,a2,a3] is equal to i_th element in [b1,b2,b3]
+    @assert [a1, a2, a3][j] == [b1, b2, b3][i] ("Opps, something is wrong, the j_th item in
+       Alice's answer does not match the i_th item in Bob's answer")
 
-    println("To reveal some magic behind the scenes, you may check that measurements on each row and each columns commute with each other and all are hermitian")
-    @assert all(k -> iscommute(op_mtx[k, :]...), 1:3) && all(k -> iscommute(op_mtx[:, k]...), 1:3) && all(ishermitian, op_mtx)
+    # To reveal some magic behind the scenes, you may check that
+    # measurements on each row and each columns commute with
+    # each other and all are hermitian
+    @assert (all(k -> iscommute(op_mtx[k, :]...), 1:3) &&
+             all(k -> iscommute(op_mtx[:, k]...), 1:3) &&
+             all(ishermitian, op_mtx))
 end
+
 
 main()
